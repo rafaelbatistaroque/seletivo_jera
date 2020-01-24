78dcd19531f9ff4c06d8457fb8992e809a562d03
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-24 13:03:03 -0400

    Valores de api atualizados.

--- lib/Api/api_moviedb.dart
+++ lib/Api/api_moviedb.dart
@@ -1,3 +1,6 @@
 class API {
   static String urlBaseMovieDb = 'https://api.themoviedb.org/3/';
+  static String apiKey = '56c2606210ce64eb0091aaf5663ba042';
+  static String novoToken = 'authentication/token/new';
+  static String login = 'authentication/token/validate_with_login';
 }
