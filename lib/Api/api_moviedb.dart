bcd3265a75752fad00cc7ac19ab88ac85f23fae4
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-24 14:42:01 -0400

    Adicionado atributos.

--- lib/Api/api_moviedb.dart
+++ lib/Api/api_moviedb.dart
@@ -3,4 +3,6 @@ class API {
   static String apiKey = '56c2606210ce64eb0091aaf5663ba042';
   static String novoToken = 'authentication/token/new';
   static String login = 'authentication/token/validate_with_login';
+  static String idioma = 'pt-BR';
+  static bool adulto = false;
 }
