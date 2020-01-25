0bf6fb65791f821575c48dde6840e508c1ca6842
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-25 00:08:13 -0400

    Criadas funções login.

---
+++ lib/repositories/login_rep.dart
@@ -0,0 +1,35 @@
+import 'package:seletivo_jera/Api/api_moviedb.dart';
+import 'package:seletivo_jera/models/login_m.dart';
+import '../services/dio_singleton.dart';
+import 'package:dio/dio.dart';
+
+class LoginRep {
+  Future<String> obterToken() async {
+    Response response = await DioSingleton().dio.get('authentication/token/new',
+        queryParameters: {'api_key': API.apiKey});
+    return response.data['request_token'];
+  }
+
+  Future<Login> autenticarUsuario(Login usuarioLogin) async {
+    Response response = await DioSingleton().dio.post(
+      'authentication/token/validate_with_login',
+      queryParameters: {
+        'api_key': API.apiKey,
+      },
+      data: {
+        "username": usuarioLogin.usuario,
+        "password": usuarioLogin.senha,
+        "request_token": usuarioLogin.token
+      },
+    );
+    return Login.fromJson(response.data);
+  }
+
+  Future<String> obterIdSecao(String token) async {
+    Response response = await DioSingleton().dio.post(
+        'authentication/session/new',
+        queryParameters: {'api_key': API.apiKey},
+        data: {'request_token': token});
+    return response.data['session_id'];
+  }
+}
