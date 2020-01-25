0d7a93f828c3cce8596daf421a040f2dfc37d16a
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-25 00:13:35 -0400

    adicionado atributo no loginModel

--- lib/models/login_m.dart
+++ lib/models/login_m.dart
@@ -2,17 +2,15 @@ class Login {
   String usuario;
   String senha;
   String token;
+  String idSecao;
 
-  Login({
-    this.usuario,
-    this.senha,
-    this.token,
-  });
+  Login({this.usuario, this.senha, this.token, this.idSecao});
 
   Login.fromJson(Map<String, dynamic> json) {
     usuario = json['username'];
     senha = json['password'];
     token = json['request_token'];
+    idSecao = json['session_id'];
   }
 
   Map<String, dynamic> toJson() {
@@ -20,6 +18,7 @@ class Login {
     data['username'] = this.usuario;
     data['password'] = this.senha;
     data['request_token'] = this.token;
+    data['session_id'] = this.idSecao;
     return data;
   }
 }
