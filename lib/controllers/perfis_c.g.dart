86fb34973d981958e0be3bc9deac90734455d192
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-26 23:16:02 -0400

    codeGen do controller do perfil

---
+++ lib/controllers/perfis_c.g.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'perfis_c.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$PerfilController on PerfilControllerBase, Store {
+  final _$listaUsuarioAtom = Atom(name: 'PerfilControllerBase.listaUsuario');
+
+  @override
+  ObservableList<dynamic> get listaUsuario {
+    _$listaUsuarioAtom.context.enforceReadPolicy(_$listaUsuarioAtom);
+    _$listaUsuarioAtom.reportObserved();
+    return super.listaUsuario;
+  }
+
+  @override
+  set listaUsuario(ObservableList<dynamic> value) {
+    _$listaUsuarioAtom.context.conditionallyRunInAction(() {
+      super.listaUsuario = value;
+      _$listaUsuarioAtom.reportChanged();
+    }, _$listaUsuarioAtom, name: '${_$listaUsuarioAtom.name}_set');
+  }
+
+  final _$carregarPerfilPadraoAsyncAction = AsyncAction('carregarPerfilPadrao');
+
+  @override
+  Future carregarPerfilPadrao(Login usuarioAtual) {
+    return _$carregarPerfilPadraoAsyncAction
+        .run(() => super.carregarPerfilPadrao(usuarioAtual));
+  }
+
+  final _$deletarDadosAsyncAction = AsyncAction('deletarDados');
+
+  @override
+  Future deletarDados() {
+    return _$deletarDadosAsyncAction.run(() => super.deletarDados());
+  }
+
+  final _$PerfilControllerBaseActionController =
+      ActionController(name: 'PerfilControllerBase');
+
+  @override
+  dynamic adicionarNovoPerfil(
+      String nome, String email, String senha, String dataNasc) {
+    final _$actionInfo = _$PerfilControllerBaseActionController.startAction();
+    try {
+      return super.adicionarNovoPerfil(nome, email, senha, dataNasc);
+    } finally {
+      _$PerfilControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+}
