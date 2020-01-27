8f63df24e9d75d8c35a15a6f93b3d1a056d7bf03
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-26 23:19:51 -0400

    refatorado login p/ adaptação com tela de perfil

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,84 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:seletivo_jera/controllers/login.c.dart';
+import 'package:seletivo_jera/models/login_m.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
+class LoginPage extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
-  LoginController loginC = LoginController();
-  var usuario, senha;
 
   @override
   Widget build(BuildContext context) {
+    final loginAtual = Provider.of<Login>(context);
+    final loginController = Provider.of<LoginController>(context);
+
     return Scaffold(
-      body: _telaLogin(),
-    );
-  }
-
-  Widget _telaLogin() {
-    return SafeArea(
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(10),
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            SizedBox(height: 100),
-            Text('login',
-                style: TextStyle(color: Colors.green, fontSize: 28),
-                textAlign: TextAlign.center),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(labelText: "Usuário"),
-              style: TextStyle(fontSize: 20),
-              validator: (value) {
-                if (value.isEmpty) return 'Usuário Inválido';
-                return null;
-              },
-              onSaved: (inputBoxUsuario) {
-                usuario = inputBoxUsuario;
-              },
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(labelText: "Senha"),
-              style: TextStyle(
-                fontSize: 20,
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.all(10),
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              SizedBox(height: 100),
+              Text('login',
+                  style: TextStyle(color: Colors.green, fontSize: 28),
+                  textAlign: TextAlign.center),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                initialValue: 'maranata',
+                decoration: InputDecoration(labelText: "Usuário"),
+                style: TextStyle(fontSize: 20),
+                validator: (value) {
+                  if (value.isEmpty) return 'Usuário Inválido';
+                  return null;
+                },
+                onSaved: (inputBoxUsuario) {
+                  loginAtual.usuario = inputBoxUsuario;
+                },
               ),
-              validator: (value) {
-                if (value.isEmpty) return 'Senha Inválida';
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                initialValue: 'adm',
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Senha"),
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) return 'Senha Inválida';
 
-                return null;
-              },
-              onSaved: (inputBoxSenha) {
-                senha = inputBoxSenha;
-              },
-            ),
-            SizedBox(height: 20),
-            RaisedButton(
-              padding: EdgeInsets.all(10),
-              color: Colors.green,
-              child: Text('Acessar'),
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                  loginC.realizarLogin(usuario, senha, context);
-                }
-              },
-            ),
-            SizedBox(height: 10),
-            Align(
-                alignment: Alignment.centerRight,
-                child: FlatButton(
-                  child: Text('Não sou cadastrado'),
-                  onPressed: () {},
-                ))
-          ],
+                  return null;
+                },
+                onSaved: (inputBoxSenha) {
+                  loginAtual.senha = inputBoxSenha;
+                },
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                padding: EdgeInsets.all(10),
+                color: Colors.green,
+                child: Text('Acessar'),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    loginController.realizarLogin(loginAtual, context);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
