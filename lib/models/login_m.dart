9171e6726c347e2e2e0b0a8d3eac1ba0ff5e3c24
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-27 09:25:56 -0400

    refatorado | adicionado atributo idSecao

--- lib/models/login_m.dart
+++ lib/models/login_m.dart
@@ -1,18 +1,18 @@
-import 'package:mobx/mobx.dart';
-part 'login_m.g.dart';
-
 class Login {
   String usuario;
   String senha;
   String token;
+  String idSecao;
   int qntPerfis;
 
-  Login({this.usuario, this.senha, this.token, this.qntPerfis = 0});
+  Login(
+      {this.usuario, this.senha, this.token, this.idSecao, this.qntPerfis = 0});
 
   Login.fromJson(Map<String, dynamic> json) {
     usuario = json['username'];
     senha = json['password'];
     token = json['request_token'];
+    idSecao = json['session_id'];
     qntPerfis = json['qnt_perfis'];
   }
   Map<String, dynamic> toJson() {
@@ -20,6 +20,7 @@ class Login {
     data['username'] = this.usuario;
     data['password'] = this.senha;
     data['request_token'] = this.token;
+    data['session_id'] = this.idSecao;
     data['qnt_perfis'] = this.qntPerfis;
     return data;
   }
