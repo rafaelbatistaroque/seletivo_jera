88948a41a9a0529c21ccc998e1503a909b697845
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-26 23:26:20 -0400

    refatorado classe login

--- lib/models/login_m.dart
+++ lib/models/login_m.dart
@@ -1,24 +1,26 @@
+import 'package:mobx/mobx.dart';
+part 'login_m.g.dart';
+
 class Login {
   String usuario;
   String senha;
   String token;
-  String idSecao;
+  int qntPerfis;
 
-  Login({this.usuario, this.senha, this.token, this.idSecao});
+  Login({this.usuario, this.senha, this.token, this.qntPerfis = 0});
 
   Login.fromJson(Map<String, dynamic> json) {
     usuario = json['username'];
     senha = json['password'];
     token = json['request_token'];
-    idSecao = json['session_id'];
+    qntPerfis = json['qnt_perfis'];
   }
-
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['username'] = this.usuario;
     data['password'] = this.senha;
     data['request_token'] = this.token;
-    data['session_id'] = this.idSecao;
+    data['qnt_perfis'] = this.qntPerfis;
     return data;
   }
 }
