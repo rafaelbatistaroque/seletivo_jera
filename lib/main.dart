808197c720626222781cf275739fb86ccf5b532f
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-26 23:09:10 -0400

    adicionado pacote provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:seletivo_jera/controllers/usuario_c.dart';
+import 'package:seletivo_jera/models/login_m.dart';
+import 'package:seletivo_jera/models/usuario_m.dart';
+import 'package:seletivo_jera/repositories/login_rep.dart';
 import 'package:seletivo_jera/views/login.dart';
+
+import 'controllers/login.c.dart';
 // import './view/login.dart';
 
 void main() => runApp(MeuApp());
@@ -7,9 +14,19 @@ void main() => runApp(MeuApp());
 class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+    return MultiProvider(
+      providers: [
+        // Provider<PerfilController>(create: (_) => PerfilController()),
+        Provider<UsuarioController>(create: (_) => UsuarioController()),
+        Provider<Usuario>(create: (_) => Usuario()),
+        Provider<LoginController>(create: (_) => LoginController()),
+        Provider<Login>(create: (_) => Login()),
+        Provider<LoginRep>(create: (_) => LoginRep())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+      ),
     );
   }
 }
